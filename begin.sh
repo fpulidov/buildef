f1aca2fd0f0e809b32d76a83dfd2189204c5801d
fpulidov <fjpulido@emergya.com>
2015-12-21 09:28:23 +0100

    prueba2

--- begin.sh
+++ begin.sh
@@ -7,13 +7,13 @@ installNode() {
   if ! [ isInstalled 'npm' ] ; then
     echo "Node is not installed. Installing..."
     curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-  fi
+s  fi
 }
 
 installBower()
 {
 
-   if ! [ isInstalled 'npm' ] ; then
+   if ! [ isInstalled "npm" ] ; then
      echo "Npm is not installed. Installing..."
      curl -L https://npmjs.org/install.sh | sh
    else
